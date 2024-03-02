0dce5eeab312ef804be02904e9f429fa831b3ca5
Seyran <106960685+NaShAdIR@users.noreply.github.com>
2024-03-03 00:28:24 +0300

    Update main.c

--- List/main.c
+++ List/main.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 #include "list.h"
 
 
-int add(int a, int b)
+static int
+add(int a, int b)
 {
     return a + b;
 }
@@ -9,17 +11,19 @@ int add(int a, int b)
 
 int main(void)
 {
+    int a = 0;
+    int b = 111;
+
     List data;
     init(&data);
-
-    int a = 0;
-    int b = 1;
 
     data.append(&data, &a);
     data.append(&data, add);
     data.append(&data, &b);
-    
-    data.delete(&data, 2);
+    data.remove(&data, 0);
+
+    int (*fn)(int, int) = data.get(&data, 0);
+    printf("%p %p\n", fn, add);
 
     return 0;
 }
