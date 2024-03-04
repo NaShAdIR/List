f106a775f5e285bd7717aff684e8626a5d3ae38a
Seyran <106960685+NaShAdIR@users.noreply.github.com>
2024-03-04 22:17:04 +0300

    Update main.c

--- List/main.c
+++ List/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "list.h"
 
+#define VOIDP(pointer) (void*)pointer
+#define FN_TYPE(return_type, arg_1, arg_2, pointer) \
+               (return_type (*)(arg_1, arg_2))pointer
 
-static int
+
+internal int
 add(int a, int b)
-{
+{    
     return a + b;
 }
 
@@ -13,18 +17,26 @@ int main(void)
 {
     int a = 0;
     int b = 111;
+    int (*fn)(int, int);
 
     List data;
     init(&data);
 
     data.append(&data, &a);
-    data.append(&data, add);
+    data.append(&data, VOIDP(add));
     data.append(&data, &b);
     data.remove(&data, 0);
 
-    int (*fn)(int, int) = data.get(&data, 0);
-    printf("%p %p\n", fn, add);
+    fn = FN_TYPE(
+        int, 
+        int, 
+        int, 
+        data.get(&data, 0)
+    );
 
+    printf("%p %p\n", VOIDP(fn), VOIDP(add));
+    printf("%d\n", fn(1, 1));
+    
     return 0;
 }
 
