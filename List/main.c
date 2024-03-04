be276e80dcccfbda467a1e84fa605d6a4ee9dcc5
Seyran <106960685+NaShAdIR@users.noreply.github.com>
2024-03-04 22:35:56 +0300

    Update main.c

--- List/main.c
+++ List/main.c
@@ -4,7 +4,7 @@
 #define VOIDP(pointer) (void*)pointer
 #define FN_TYPE(return_type, arg_1, arg_2, pointer) \
                (return_type (*)(arg_1, arg_2))pointer
-
+    
 
 internal int
 add(int a, int b)
@@ -13,29 +13,36 @@ add(int a, int b)
 }
 
 
-int main(void)
+internal void 
+dataHendler(List data)
 {
-    int a = 0;
-    int b = 111;
     int (*fn)(int, int);
-
-    List data;
-    init(&data);
-
-    data.append(&data, &a);
-    data.append(&data, VOIDP(add));
-    data.append(&data, &b);
-    data.remove(&data, 0);
-
     fn = FN_TYPE(
-        int, 
-        int, 
-        int, 
+        int,
+        int,
+        int,
         data.get(&data, 0)
     );
 
     printf("%p %p\n", VOIDP(fn), VOIDP(add));
     printf("%d\n", fn(1, 1));
+}
+
+
+int main(void)
+{
+    List data;
+
+    int a = 0;
+    int b = 111;
+    
+    init(&data);
+    data.append(&data, &a);
+    data.append(&data, VOIDP(add));
+    data.append(&data, &b);
+    data.remove(&data, 0);
+    
+    dataHendler(data);
     
     return 0;
 }
